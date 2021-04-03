0ab0a9559532f6b2f9f7f5f3f337a10e396fb7fe
mostafizurbappy <mostafizur15-8845@diu.edu.bd.com>
2021-04-04 00:30:33 +0600

    LS-37POINT9

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from'../../fakeData'
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import'./Shop.css'
@@ -13,6 +14,9 @@ const Shop = () => {
         
         const newCart = [...cart, product];
         setCart(newCart);
+        const sameProduct = newCart.filter(pd => pd.key === product.key);
+        const count = sameProduct.length ;
+        addToDatabaseCart(product.key, count);
     }
 
     return (
@@ -21,6 +25,7 @@ const Shop = () => {
                 
                 {
                   products.map(pd => <Product 
+                    key = {pd.key}
                     showAddToCart={true}
                     handleAddProduct = {handleAddProduct}
                     product={pd}
